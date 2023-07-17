2aabca8967e0c8752d4aa3861084078502391fbb
Marcos Martins <marcos.martins@sncf.fr>
2023-07-17 11:30:27 +0200

    my first spring

---
+++ src/main/java/com/ChallengeDoctor/challengeDoctor/ChallengeDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.ChallengeDoctor.challengeDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChallengeDoctor/challengeDoctor/ChallengeDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChallengeDoctor.challengeDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
